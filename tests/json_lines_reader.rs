9b367d708af0752e11594400f1820cec1e7a4f15
John T. Wodder II <git@varonathe.org>
2022-10-27 08:54:52 -0400

    More tests of JsonLinesReader

--- tests/json_lines_reader.rs
+++ tests/json_lines_reader.rs
@@ -3,7 +3,7 @@ use assert_fs::NamedTempFile;
 use jsonlines::JsonLinesReader;
 use serde::Deserialize;
 use std::fs::File;
-use std::io::{BufReader, Result};
+use std::io::{BufRead, BufReader, ErrorKind, Result};
 use std::path::Path;
 
 #[derive(Debug, Deserialize, Eq, PartialEq)]
@@ -11,6 +11,12 @@ struct Structure {
     name: String,
     size: i32,
     on: bool,
+}
+
+#[derive(Debug, Deserialize, Eq, PartialEq)]
+struct Point {
+    x: i32,
+    y: i32,
 }
 
 static DATA_DIR: &str = concat!(env!("CARGO_MANIFEST_DIR"), "/tests/data");
@@ -24,6 +30,20 @@ fn test_read_empty() {
     assert_eq!(reader.read::<Structure>().unwrap(), None);
     assert_eq!(reader.read::<Structure>().unwrap(), None);
     assert_eq!(reader.read::<Structure>().unwrap(), None);
+}
+
+#[test]
+fn test_read_one() {
+    let fp = BufReader::new(File::open(Path::new(DATA_DIR).join("sample01.jsonl")).unwrap());
+    let mut reader = JsonLinesReader::new(fp);
+    assert_eq!(
+        reader.read::<Structure>().unwrap(),
+        Some(Structure {
+            name: "Foo Bar".into(),
+            size: 42,
+            on: true,
+        })
+    );
 }
 
 #[test]
@@ -86,4 +106,98 @@ fn test_iter_collect() {
             },
         ]
     );
+}
+
+#[test]
+fn test_read_one_then_read_inner() {
+    let fp = BufReader::new(File::open(Path::new(DATA_DIR).join("sample02.txt")).unwrap());
+    let mut reader = JsonLinesReader::new(fp);
+    assert_eq!(
+        reader.read::<Structure>().unwrap(),
+        Some(Structure {
+            name: "Foo Bar".into(),
+            size: 42,
+            on: true,
+        })
+    );
+    let mut fp: BufReader<File> = reader.into_inner();
+    let mut s = String::new();
+    fp.read_line(&mut s).unwrap();
+    assert_eq!(s, "Not JSON.\n");
+}
+
+#[test]
+fn test_read_two() {
+    let fp = BufReader::new(File::open(Path::new(DATA_DIR).join("sample03.jsonl")).unwrap());
+    let mut reader = JsonLinesReader::new(fp);
+    assert_eq!(
+        reader.read::<Structure>().unwrap(),
+        Some(Structure {
+            name: "Foo Bar".into(),
+            size: 42,
+            on: true,
+        })
+    );
+    assert_eq!(
+        reader.read::<Point>().unwrap(),
+        Some(Point { x: 69, y: 105 })
+    );
+}
+
+#[test]
+fn test_iter_invalid_json() {
+    let fp = BufReader::new(File::open(Path::new(DATA_DIR).join("sample04.txt")).unwrap());
+    let reader = JsonLinesReader::new(fp);
+    let mut items = reader.iter::<Structure>();
+    assert_eq!(
+        items.next().unwrap().unwrap(),
+        Structure {
+            name: "Foo Bar".into(),
+            size: 42,
+            on: true,
+        }
+    );
+
+    let e = items.next().unwrap().unwrap_err();
+    assert_eq!(e.kind(), ErrorKind::UnexpectedEof);
+    assert!(e
+        .into_inner()
+        .unwrap()
+        .downcast::<serde_json::Error>()
+        .is_ok());
+
+    assert_eq!(
+        items.next().unwrap().unwrap(),
+        Structure {
+            name: "Quux".into(),
+            size: 23,
+            on: false,
+        }
+    );
+
+    let e = items.next().unwrap().unwrap_err();
+    assert_eq!(e.kind(), ErrorKind::InvalidData);
+    assert!(e
+        .into_inner()
+        .unwrap()
+        .downcast::<serde_json::Error>()
+        .is_ok());
+
+    let e = items.next().unwrap().unwrap_err();
+    assert_eq!(e.kind(), ErrorKind::InvalidData);
+    assert!(e
+        .into_inner()
+        .unwrap()
+        .downcast::<serde_json::Error>()
+        .is_ok());
+
+    assert_eq!(
+        items.next().unwrap().unwrap(),
+        Structure {
+            name: "Gnusto Cleesh".into(),
+            size: 17,
+            on: true,
+        }
+    );
+    assert!(items.next().is_none())
 }
