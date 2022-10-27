004c1c6bb4fdab8fd6caab48d6728046f18d079e
John T. Wodder II <git@varonathe.org>
2022-10-26 21:47:23 -0400

    JsonLinesReader

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
-use serde::Serialize;
-use std::io::{Result, Write};
+use serde::{de::DeserializeOwned, Serialize};
+use std::io::{BufRead, Result, Write};
+use std::marker::PhantomData;
 
 #[derive(Debug)]
 pub struct JsonLinesWriter<W> {
@@ -39,5 +40,60 @@ impl<W: Write> JsonLinesWriter<W> {
 
     pub fn flush(&mut self) -> Result<()> {
         self.inner.flush()
+    }
+}
+
+#[derive(Debug)]
+pub struct JsonLinesReader<R> {
+    inner: R,
+}
+
+impl<R> JsonLinesReader<R> {
+    pub fn new(reader: R) -> Self {
+        JsonLinesReader { inner: reader }
+    }
+
+    pub fn into_inner(self) -> R {
+        self.inner
+    }
+}
+
+impl<R: BufRead> JsonLinesReader<R> {
+    pub fn read<T>(&mut self) -> Result<Option<T>>
+    where
+        T: DeserializeOwned,
+    {
+        let mut s = String::new();
+        let r = self.inner.read_line(&mut s)?;
+        if r == 0 {
+            Ok(None)
+        } else {
+            Ok(Some(serde_json::from_str::<T>(&s)?))
+        }
+    }
+
+    pub fn iter<T>(self) -> Iter<R, T> {
+        Iter {
+            reader: self,
+            _output: PhantomData,
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct Iter<R, T> {
+    reader: JsonLinesReader<R>,
+    _output: PhantomData<T>,
+}
+
+impl<R, T> Iterator for Iter<R, T>
+where
+    T: DeserializeOwned,
+    R: BufRead,
+{
+    type Item = Result<T>;
+
+    fn next(&mut self) -> Option<Result<T>> {
+        self.reader.read().transpose()
     }
 }
