02500cc04701520f953f6c068b4e7465041b0c7d
John T. Wodder II <git@varonathe.org>
2022-10-28 12:14:51 -0400

    Example code for append_json_lines()

--- src/lib.rs
+++ src/lib.rs
@@ -532,6 +532,71 @@ where
 /// Has the same error conditions as [`File::create()`],
 /// [`serde_json::to_writer()`], [`std::io::Write::write_all()`], and
 /// [`std::io::Write::flush()`].
+///
+/// # Example
+///
+/// ```no_run
+/// use jsonlines::append_json_lines;
+/// use serde::Serialize;
+/// use std::fs::read_to_string;
+///
+/// #[derive(Serialize)]
+/// pub struct Structure {
+///     pub name: String,
+///     pub size: i32,
+///     pub on: bool,
+/// }
+///
+/// fn main() -> std::io::Result<()> {
+///     append_json_lines(
+///         "example.jsonl",
+///         [
+///             Structure {
+///                 name: "Foo Bar".into(),
+///                 size: 42,
+///                 on: true,
+///             },
+///             Structure {
+///                 name: "Quux".into(),
+///                 size: 23,
+///                 on: false,
+///             },
+///         ],
+///     )?;
+///     assert_eq!(
+///         read_to_string("example.jsonl")?,
+///         concat!(
+///             "{\"name\":\"Foo Bar\",\"size\":42,\"on\":true}\n",
+///             "{\"name\":\"Quux\",\"size\":23,\"on\":false}\n",
+///         )
+///     );
+///     append_json_lines(
+///         "example.jsonl",
+///         [
+///             Structure {
+///                 name: "Gnusto Cleesh".into(),
+///                 size: 17,
+///                 on: true,
+///             },
+///             Structure {
+///                 name: "baz".into(),
+///                 size: 69105,
+///                 on: false,
+///             },
+///         ],
+///     )?;
+///     assert_eq!(
+///         read_to_string("example.jsonl")?,
+///         concat!(
+///             "{\"name\":\"Foo Bar\",\"size\":42,\"on\":true}\n",
+///             "{\"name\":\"Quux\",\"size\":23,\"on\":false}\n",
+///             "{\"name\":\"Gnusto Cleesh\",\"size\":17,\"on\":true}\n",
+///             "{\"name\":\"baz\",\"size\":69105,\"on\":false}\n",
+///         )
+///     );
+///     Ok(())
+/// }
+/// ```
 pub fn append_json_lines<P, I, T>(path: P, items: I) -> Result<()>
 where
     P: AsRef<Path>,
