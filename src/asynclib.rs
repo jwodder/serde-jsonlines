60af5360fe0d2f0ea89eb91ea1161d1e623a883b
John T. Wodder II <git@varonathe.org>
2022-10-30 00:03:43 -0400

    AsyncJsonLinesReader::read_all()

--- src/asynclib.rs
+++ src/asynclib.rs
@@ -2,9 +2,12 @@
 use pin_project_lite::pin_project;
 use serde::de::DeserializeOwned;
 use std::io::Result;
+use std::marker::PhantomData;
 use std::marker::Unpin;
 use std::pin::Pin;
-use tokio::io::{AsyncBufRead, AsyncBufReadExt};
+use std::task::{Context, Poll};
+use tokio::io::{AsyncBufRead, AsyncBufReadExt, Lines};
+use tokio_stream::Stream;
 
 pin_project! {
     #[derive(Debug)]
@@ -42,10 +45,11 @@ impl<R> AsyncJsonLinesReader<R> {
     }
 }
 
-impl<R: AsyncBufRead + Unpin> AsyncJsonLinesReader<R> {
+impl<R: AsyncBufRead> AsyncJsonLinesReader<R> {
     pub async fn read<T>(&mut self) -> Result<Option<T>>
     where
         T: DeserializeOwned,
+        R: Unpin,
     {
         let mut s = String::new();
         let r = self.inner.read_line(&mut s).await?;
@@ -53,6 +57,40 @@ impl<R: AsyncBufRead + Unpin> AsyncJsonLinesReader<R> {
             Ok(None)
         } else {
             Ok(Some(serde_json::from_str::<T>(&s)?))
+        }
+    }
+
+    pub fn read_all<T>(self) -> JsonLinesStream<R, T> {
+        JsonLinesStream {
+            inner: self.inner.lines(),
+            _output: PhantomData,
+        }
+    }
+}
+
+pin_project! {
+    #[derive(Debug)]
+    pub struct JsonLinesStream<R, T> {
+        #[pin]
+        inner: Lines<R>,
+        _output: PhantomData<T>,
+    }
+}
+
+impl<R: AsyncBufRead, T> Stream for JsonLinesStream<R, T>
+where
+    T: DeserializeOwned,
+{
+    type Item = Result<T>;
+
+    fn poll_next(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
+        match self.project().inner.poll_next_line(cx) {
+            Poll::Pending => Poll::Pending,
+            Poll::Ready(Ok(Some(line))) => {
+                Some(serde_json::from_str::<T>(&line).map_err(Into::into)).into()
+            }
+            Poll::Ready(Ok(None)) => None.into(),
+            Poll::Ready(Err(e)) => Some(Err(e)).into(),
         }
     }
 }
--- tests/async_json_lines_reader.rs
+++ tests/async_json_lines_reader.rs
@@ -2,13 +2,14 @@
 use assert_fs::fixture::{FileTouch, FileWriteStr};
 use assert_fs::NamedTempFile;
 use serde_jsonlines::AsyncJsonLinesReader;
-use std::io::SeekFrom;
+use std::io::{ErrorKind, Result, SeekFrom};
 use std::path::Path;
 use std::pin::Pin;
 use tokio::fs::{File, OpenOptions};
 use tokio::io::{AsyncBufReadExt, AsyncSeekExt, AsyncWriteExt, BufReader};
 mod common;
 use common::*;
+use tokio_stream::StreamExt;
 
 #[tokio::test]
 async fn test_read_empty() {
@@ -150,4 +151,139 @@ async fn test_read_one_then_read_pin_mut() {
     let mut s = String::new();
     fp.read_line(&mut s).await.unwrap();
     assert_eq!(s, "Not JSON.\n");
+}
+
+#[tokio::test]
+async fn test_read_all() {
+    let fp = BufReader::new(
+        File::open(Path::new(DATA_DIR).join("sample01.jsonl"))
+            .await
+            .unwrap(),
+    );
+    let reader = AsyncJsonLinesReader::new(fp);
+    let items = reader.read_all::<Structure>();
+    tokio::pin!(items);
+    assert_eq!(
+        items.next().await.unwrap().unwrap(),
+        Structure {
+            name: "Foo Bar".into(),
+            size: 42,
+            on: true,
+        }
+    );
+    assert_eq!(
+        items.next().await.unwrap().unwrap(),
+        Structure {
+            name: "Quux".into(),
+            size: 23,
+            on: false,
+        }
+    );
+    assert_eq!(
+        items.next().await.unwrap().unwrap(),
+        Structure {
+            name: "Gnusto Cleesh".into(),
+            size: 17,
+            on: true,
+        }
+    );
+    assert!(items.next().await.is_none());
+}
+
+#[tokio::test]
+async fn test_read_all_collect() {
+    let fp = BufReader::new(
+        File::open(Path::new(DATA_DIR).join("sample01.jsonl"))
+            .await
+            .unwrap(),
+    );
+    let reader = AsyncJsonLinesReader::new(fp);
+    let items = reader
+        .read_all::<Structure>()
+        .collect::<Result<Vec<_>>>()
+        .await
+        .unwrap();
+    assert_eq!(
+        items,
+        [
+            Structure {
+                name: "Foo Bar".into(),
+                size: 42,
+                on: true,
+            },
+            Structure {
+                name: "Quux".into(),
+                size: 23,
+                on: false,
+            },
+            Structure {
+                name: "Gnusto Cleesh".into(),
+                size: 17,
+                on: true,
+            },
+        ]
+    );
+}
+
+#[tokio::test]
+async fn test_read_all_invalid_json() {
+    let fp = BufReader::new(
+        File::open(Path::new(DATA_DIR).join("sample04.txt"))
+            .await
+            .unwrap(),
+    );
+    let reader = AsyncJsonLinesReader::new(fp);
+    let items = reader.read_all::<Structure>();
+    tokio::pin!(items);
+    assert_eq!(
+        items.next().await.unwrap().unwrap(),
+        Structure {
+            name: "Foo Bar".into(),
+            size: 42,
+            on: true,
+        }
+    );
+
+    let e = items.next().await.unwrap().unwrap_err();
+    assert_eq!(e.kind(), ErrorKind::UnexpectedEof);
+    assert!(e
+        .into_inner()
+        .unwrap()
+        .downcast::<serde_json::Error>()
+        .is_ok());
+
+    assert_eq!(
+        items.next().await.unwrap().unwrap(),
+        Structure {
+            name: "Quux".into(),
+            size: 23,
+            on: false,
+        }
+    );
+
+    let e = items.next().await.unwrap().unwrap_err();
+    assert_eq!(e.kind(), ErrorKind::InvalidData);
+    assert!(e
+        .into_inner()
+        .unwrap()
+        .downcast::<serde_json::Error>()
+        .is_ok());
+
+    let e = items.next().await.unwrap().unwrap_err();
+    assert_eq!(e.kind(), ErrorKind::InvalidData);
+    assert!(e
+        .into_inner()
+        .unwrap()
+        .downcast::<serde_json::Error>()
+        .is_ok());
+
+    assert_eq!(
+        items.next().await.unwrap().unwrap(),
+        Structure {
+            name: "Gnusto Cleesh".into(),
+            size: 17,
+            on: true,
+        }
+    );
+    assert!(items.next().await.is_none());
 }
